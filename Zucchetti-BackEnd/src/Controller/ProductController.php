658ea6f3ae81e649d3e6feac0c80c8974568ccc1
Drew <84882983+vdr3w@users.noreply.github.com>
2024-04-27 21:34:10 -0300

    feat: Add create product functionality
    Implemented functionality to create new products via a POST request, handling JSON input to register product details.

---
+++ Zucchetti-BackEnd/src/Controller/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MyProject\Controller;
+
+use Doctrine\ORM\EntityManager;
+use MyProject\Entity\Product;
+
+class ProductController
+{
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function createProduct()
+    {
+        // Capturando o conteúdo JSON da requisição
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data['name'], $data['price'], $data['quantity'])) {
+            http_response_code(400);
+            return "Missing data for name, price or quantity.";
+        }
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice((float) $data['price']);
+        $product->setQuantity((int) $data['quantity']);
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        http_response_code(201);
+        return "Created Product with ID " . $product->getId() . "\n";
+    }
+
+}
