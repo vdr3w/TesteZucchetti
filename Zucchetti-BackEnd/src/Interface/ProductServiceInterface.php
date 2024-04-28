17e9fac973e2069ba353540b36b41254480644ab
Drew <84882983+vdr3w@users.noreply.github.com>
2024-04-28 08:28:50 -0300

    feat: implement dependency inversion for product service
    Updated ProductServiceInterface and ProductService to enforce dependency inversion principle.

--- Zucchetti-BackEnd/src/Interface/ProductServiceInterface.php
+++ Zucchetti-BackEnd/src/Interface/ProductServiceInterface.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace MyProject\Interface;
 
-use MyProject\Entity\Product;
-
 interface ProductServiceInterface {
-    public function createProduct(array $data): Product;
+    public function createProduct(array $data): array;
     public function listProducts(): array;
-    public function showProduct(int $id): ?Product;
-    public function updateProduct(int $id, array $data): Product;
-    public function deleteProduct(int $id): void;
+    public function showProduct(int $id): array;
+    public function updateProduct(int $id, array $data): array;
+    public function deleteProduct(int $id): array;
 }
