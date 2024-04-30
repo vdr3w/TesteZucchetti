484933770488359991496116fa765299c14e60f6
Drew <84882983+vdr3w@users.noreply.github.com>
2024-04-30 16:26:34 -0300

    feat: Add authentication system and related views
    Implemented a new authentication system including login and unauthorized views. Updated router to handle new authentication paths.

---
+++ Zucchetti-FrontEnd/src/views/UnauthorizedView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="unauthorized-container">
+      <h1>Acesso Negado</h1>
+      <p>Você não tem permissão para acessar esta página.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  }
+  </script>
+  
+  <style scoped>
+  .unauthorized-container {
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
