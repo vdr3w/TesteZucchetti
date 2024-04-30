e3e974363b2e84b890d69fd21f2eef01528ed6bd
Drew <84882983+vdr3w@users.noreply.github.com>
2024-04-30 07:25:36 -0300

    feat: Implement authentication routes and middleware
    Set up routes.php to include JWT authentication checks and configured route handling for authenticated operations.

--- Zucchetti-BackEnd/routes.php
+++ Zucchetti-BackEnd/routes.php
@@ -30,6 +30,24 @@ $customerController = new CustomerController($customerService);
 $paymentMethodController = new PaymentMethodController($paymentMethodService);
 $saleController = new SaleController($saleService);
 
+// Função de Middleware para verificar a autenticação
+function isAuthenticated()
+{
+    global $authService;
+    $headers = getallheaders();
+    $authHeader = $headers['Authorization'] ?? null;
+
+    if ($authHeader) {
+        [$bearer, $token] = explode(' ', $authHeader, 2);
+        if ($bearer === 'Bearer' && $authService->validateToken($token)) {
+            return true;
+        }
+    }
+
+    http_response_code(401);
+    echo json_encode(['error' => 'Não autenticado']);
+    exit;
+}
 
 $inputJSON = file_get_contents('php://input');
 $data = json_decode($inputJSON, true);
@@ -42,27 +60,57 @@ $path = $parsedUrl['path'];
 
 // Roteamento
 switch ($path) {
+        // LOGIN
+    case '/login':
+        if ($requestMethod == 'POST') {
+            $username = $data['username'];
+            $password = $data['password'];
+
+            $user = $entityManager->getRepository(BUser::class)->findOneBy(['username' => $username]);
+
+            if ($user && password_verify($password, $user->getPassword())) {
+                $authService->deleteTokensForUser($user);
+
+                $tokenStr = $authService->generateToken($user);
+                $token = new AuthToken();
+                $token->setUser($user);
+                $token->setToken($tokenStr);
+                $token->setCreatedAt(new \DateTime());
+
+                $authService->saveToken($token);
+
+                echo json_encode(['token' => $tokenStr]);
+            } else {
+                http_response_code(401);
+                echo "Unauthorized";
+            }
+            break;
+        }
         // ROTAS PRODUTOS
     case '/product/create':
         if ($requestMethod == 'POST') {
+            isAuthenticated();
             $response = $productController->createProduct($data['name'], $data['price'], $data['quantity']);
             echo $response;
         }
         break;
     case '/product/list':
         if ($requestMethod == 'GET') {
+            isAuthenticated();
             $response = $productController->listProducts();
             echo $response;
         }
         break;
     case '/product/show':
         if ($requestMethod == 'GET' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $productController->showProduct($_GET['id']);
             echo $response;
         }
         break;
     case '/product/update':
         if ($requestMethod == 'POST') {
+            isAuthenticated();
             $data = json_decode(file_get_contents("php://input"), true);
             if (isset($data['id'], $data['name'], $data['price'], $data['quantity'])) {
                 $response = $productController->updateProduct($data['id'], $data['name'], $data['price'], $data['quantity']);
@@ -75,6 +123,7 @@ switch ($path) {
         break;
     case '/product/delete':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $productController->deleteProduct($_GET['id']);
             echo $response;
         }
@@ -83,30 +132,35 @@ switch ($path) {
         // ROTAS CLIENTES
     case '/customer/create':
         if ($requestMethod == 'POST') {
+            isAuthenticated();
             $response = $customerController->createCustomer();
             echo $response;
         }
         break;
     case '/customer/list':
         if ($requestMethod == 'GET') {
+            isAuthenticated();
             $response = $customerController->listCustomers();
             echo $response;
         }
         break;
     case '/customer/show':
         if ($requestMethod == 'GET' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $customerController->showCustomer($_GET['id']);
             echo $response;
         }
         break;
     case '/customer/update':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $customerController->updateCustomer($_GET['id']);
             echo $response;
         }
         break;
     case '/customer/delete':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $customerController->deleteCustomer($_GET['id']);
             echo $response;
         }
@@ -115,30 +169,35 @@ switch ($path) {
         // ROTAS PARA PAYMENT METHODS
     case '/payment-method/create':
         if ($requestMethod == 'POST') {
+            isAuthenticated();
             $response = $paymentMethodController->createPaymentMethod();
             echo $response;
         }
         break;
     case '/payment-method/list':
         if ($requestMethod == 'GET') {
+            isAuthenticated();
             $response = $paymentMethodController->listPaymentMethods();
             echo $response;
         }
         break;
     case '/payment-method/show':
         if ($requestMethod == 'GET' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $paymentMethodController->showPaymentMethod($_GET['id']);
             echo $response;
         }
         break;
     case '/payment-method/update':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $paymentMethodController->updatePaymentMethod($_GET['id']);
             echo $response;
         }
         break;
     case '/payment-method/delete':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $paymentMethodController->deletePaymentMethod($_GET['id']);
             echo $response;
         }
@@ -147,18 +206,21 @@ switch ($path) {
         // Rotas para Sale
     case '/sale/create':
         if ($requestMethod == 'POST') {
+            isAuthenticated();
             $response = $saleController->createSale();
             echo $response;
         }
         break;
     case '/sale/list':
         if ($requestMethod == 'GET') {
+            isAuthenticated();
             $response = $saleController->listSales();
             echo $response;
         }
         break;
     case '/sale/listByCustomer':
         if ($requestMethod == 'GET' && isset($_GET['customerId'])) {
+            isAuthenticated();
             $customerId = $_GET['customerId'];
             $response = $saleController->listSalesByCustomer($customerId);
             echo $response;
@@ -167,49 +229,25 @@ switch ($path) {
 
     case '/sale/show':
         if ($requestMethod == 'GET' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $saleController->showSale($_GET['id']);
             echo $response;
         }
         break;
     case '/sale/update':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $saleController->updateSale($_GET['id']);
             echo $response;
         }
         break;
     case '/sale/delete':
         if ($requestMethod == 'POST' && isset($_GET['id'])) {
+            isAuthenticated();
             $response = $saleController->deleteSale($_GET['id']);
             echo $response;
         }
         break;
-
-        //
-        case '/login':
-            if ($requestMethod == 'POST') {
-                $username = $data['username'];
-                $password = $data['password'];
-    
-                $user = $entityManager->getRepository(BUser::class)->findOneBy(['username' => $username]);
-    
-                if ($user && password_verify($password, $user->getPassword())) {
-                    $authService->deleteTokensForUser($user);
-    
-                    $tokenStr = $authService->generateToken($user);
-                    $token = new AuthToken();
-                    $token->setUser($user);
-                    $token->setToken($tokenStr);
-                    $token->setCreatedAt(new \DateTime());
-    
-                    $authService->saveToken($token);
-    
-                    echo json_encode(['token' => $tokenStr]);
-                } else {
-                    http_response_code(401);
-                    echo "Unauthorized";
-                }
-                break;
-            }
     default:
         http_response_code(404);
         echo "404 Not Found";
